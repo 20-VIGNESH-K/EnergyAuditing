0b1628e8fdfb47fa9ef1fbba95af8cbd3b452296
20-VIGNESH-K <vigneshvignesh7035@gmail.com>
2024-02-01 20:26:36 +0530

    added login - frontend

--- routes/index.go
+++ routes/index.go
@@ -8,9 +8,11 @@ import (
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
-
+	// Serve static files for the register and login pages
 	router.Static("/register", "./frontend/createUser/")
+	router.Static("/login", "./frontend/login/")
 
+	// Handle POST requests for user registration and login
 	router.POST("/register", Controllers.CreateUser)
 	router.POST("/login", Controllers.Login)
 

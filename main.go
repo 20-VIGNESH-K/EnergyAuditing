ef5284f85faccfd9fdef2812e419cc8e5e163340
20-VIGNESH-K <vigneshvignesh7035@gmail.com>
2024-04-30 17:50:58 +0530

    updated for deployment

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"log"
+	"os"
+
 	"github.com/20-VIGNESH-K/EnergyAuditing/routes"
 )
 
 func main() {
 	router := routes.SetupRouter()
-	log.Fatal(router.Run(":8082"))
+	port := os.Getenv("port")
+	if port == "" {
+        port = "8080"
+    }
+	log.Fatal(router.Run(":"+port))
 }
